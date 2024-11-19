67fd9abeca86161ba01e8457f883b0e9d3e475ca
boumster <95254000+boumster@users.noreply.github.com>
2024-11-18 21:56:24 -0700

    merge main

--- Entity/BillingSystem.java
+++ Entity/BillingSystem.java
@@ -2,5 +2,5 @@ package Entity;
 
 public interface BillingSystem {
     public boolean makePayment(User user, double amount);
-    public void refundPayment(User user);
+    public void refundPayment(User user, double amount);
 }
